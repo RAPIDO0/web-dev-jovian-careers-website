0435e10a7217f280647e8f3a4ad4f2a7100aa08d
RAPIDO0 <h.gotzorides@gmail.com>
2023-09-26 15:55:37 +0200

    Dynamic Data using Templates

--- app.py
+++ app.py
@@ -2,9 +2,36 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [
+    {
+        'id': 1,
+        'title': 'Data Analyst',
+        'location': 'Paris, France',
+        'salary': ' 100K €'
+    },
+    {
+        'id': 2,
+        'title': 'Data Scientist',
+        'location': 'Paris, France',
+        'salary': ' 100 €'
+    },
+    {
+        'id': 3,
+        'title': 'Back-end Engineer',
+        'location': ' Remote',
+    },
+    {
+        'id': 4,
+        'title': 'Front-end Engineer',
+        'location': 'Remote',
+        'salary': ' 100K £'
+    }
+]
+
+
 @app.route("/") ##path after the domain name
 def hello_world():
-    return render_template('home.html')
+    return render_template('home_bootstrap.html', jobs=JOBS)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
